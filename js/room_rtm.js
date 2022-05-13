64fc7741687de411e67061f8f2d5212c7bb6f408
yuxianxu <yuxian.xu@gmail.com>
2022-05-12 23:17:35 -0700

    Add adding member notification feature

--- js/room_rtm.js
+++ js/room_rtm.js
@@ -1,4 +1,14 @@
 let handleMemberJoined = async (MemberId) => {
   console.log('A new member joined the room:', MemberId);
+  addMemberToDom(MemberId)
 };
- 
+
+let addMemberToDom = async (MemberId) => {
+    let memberWrapper = document.getElementById('member__list')
+    let memberItem = `<div class="member__wrapper" id="member__${MemberId}__wrapper">
+    <span class="green__icon"></span>
+    <p class="member_name">${MemberId}</p>
+</div>`
+
+memberWrapper.insertAdjacentHTML('beforeend', memberItem)
+}
